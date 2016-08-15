8b2f6bf4e14b9544966054f136148c25f2f00e1b
Andrew Nissen <adnissen@protonmail.com>
2016-08-15 16:36:04 -0400

    updated build and gitignore scripts

--- client/build.sh
+++ client/build.sh
@@ -5,12 +5,12 @@ mkdir build
 electron-packager . --all --out=build --overwrite
 
 echo BUILD COMPLETE, UPLOADING TO ITCH
-echo (keep an eye out, you may be prompted!)
+echo keep an eye out, you may be prompted!
 echo ...
-butler push build/ElderRune-win32-x64 ambushsabre/elder-runes:win64pre-alpha
-butler push build/ElderRune-win32-ia32 ambushsabre/elder-runes:win32pre-alpha
-butler push build/ElderRune-linux-ia32 ambushsabre/elder-runes:linux32pre-alpha
-butler push build/ElderRune-linux-x64 ambushsabre/elder-runes:linux64pre-alpha
+butler push build/ElderRune-win64 ambushsabre/elder-runes:win64pre-alpha
+butler push build/ElderRune-win32 ambushsabre/elder-runes:win32pre-alpha
+butler push build/ElderRune-linux32 ambushsabre/elder-runes:linux32pre-alpha
+butler push build/ElderRune-linux64 ambushsabre/elder-runes:linux64pre-alpha
 
 echo PUSH COMPLETE!
 echo TIME TO PARTY!!!!!!!!!!
