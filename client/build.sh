cc985fc8744c9b462dcbf32e17a6c5814dc7a0bd
Andrew Nissen <adnissen@protonmail.com>
2016-08-15 16:52:24 -0400

    updated build script a bit

--- client/build.sh
+++ client/build.sh
@@ -7,10 +7,8 @@ electron-packager . --all --out=build --overwrite
 echo BUILD COMPLETE, UPLOADING TO ITCH
 echo keep an eye out, you may be prompted!
 echo ...
-butler push build/ElderRune-win64 ambushsabre/elder-runes:win64pre-alpha
-butler push build/ElderRune-win32 ambushsabre/elder-runes:win32pre-alpha
-butler push build/ElderRune-linux32 ambushsabre/elder-runes:linux32pre-alpha
-butler push build/ElderRune-linux64 ambushsabre/elder-runes:linux64pre-alpha
+butler push build/ElderRune-win32-ia32 ambushsabre/elder-runes:win-pre-alpha
+butler push build/ElderRune-linux-ia32 ambushsabre/elder-runes:linux-pre-alpha
 
 echo PUSH COMPLETE!
 echo TIME TO PARTY!!!!!!!!!!
