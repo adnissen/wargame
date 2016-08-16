35e981f5885d262a2faa3904a1eaa8fdad79234f
Andrew Nissen <adnissen@protonmail.com>
2016-08-16 18:05:54 -0400

    fixed up a buncha stuff related to units

--- src/packages/units/units.go
+++ src/packages/units/units.go
@@ -61,9 +61,9 @@ type Weapon struct {
 	Keywords      []string
 }
 
-var UnitList = make([]Unit, 7)
-var SquadList = make([]Squad, 4)
-var WeaponList = make([]Weapon, 6)
+var UnitList []Unit
+var SquadList []Squad
+var WeaponList []Weapon
 
 func (u *Unit) SetPos(x int, y int) {
 	u.X = x
@@ -71,46 +71,74 @@ func (u *Unit) SetPos(x int, y int) {
 }
 
 func LoadWeapons() {
-	WeaponList[0] = Weapon{Id: 0, DisplayName: "Longbow", Key: "longbow", MinRng: 2, Rng: 5, Atk: 10, Dmg: 4, Uses: 15, UsesRemaining: 15}
-	WeaponList[1] = Weapon{Id: 1, DisplayName: "Hunter's Bow", Key: "longbow", MinRng: 2, Rng: 3, Atk: 11, Dmg: 2, Uses: 99, UsesRemaining: 99}
-	WeaponList[2] = Weapon{Id: 2, DisplayName: "Short Sword", Key: "short_sword", Rng: 1, Atk: 7, Dmg: 2, Uses: 99, UsesRemaining: 99}
-	WeaponList[3] = Weapon{Id: 3, DisplayName: "Lance", Key: "lance", Rng: 1, Atk: 13, Dmg: 5, Uses: 5, UsesRemaining: 5}
-	WeaponList[4] = Weapon{Id: 4, DisplayName: "Spear", Key: "spear", Rng: 1, Atk: 10, Dmg: 4, Uses: 99, UsesRemaining: 99}
-	WeaponList[5] = Weapon{Id: 5, DisplayName: "Long Sword", Key: "longsword", Rng: 1, Atk: 10, Dmg: 3, Uses: 99, UsesRemaining: 99}
+
+	AddWeapon(Weapon{Id: 0, DisplayName: "Longbow", Key: "longbow", MinRng: 2, Rng: 5, Atk: 10, Dmg: 4, Uses: 15, UsesRemaining: 15})
+	AddWeapon(Weapon{Id: 1, DisplayName: "Hunter's Bow", Key: "longbow", MinRng: 2, Rng: 3, Atk: 11, Dmg: 2, Uses: 99, UsesRemaining: 99})
+	AddWeapon(Weapon{Id: 2, DisplayName: "Short Sword", Key: "short_sword", Rng: 1, Atk: 7, Dmg: 2, Uses: 99, UsesRemaining: 99})
+	AddWeapon(Weapon{Id: 3, DisplayName: "Lance", Key: "lance", Rng: 1, Atk: 13, Dmg: 5, Uses: 5, UsesRemaining: 5})
+	AddWeapon(Weapon{Id: 4, DisplayName: "Spear", Key: "spear", Rng: 1, Atk: 10, Dmg: 4, Uses: 99, UsesRemaining: 99})
+	AddWeapon(Weapon{Id: 5, DisplayName: "Long Sword", Key: "longsword", Rng: 1, Atk: 10, Dmg: 3, Uses: 99, UsesRemaining: 99})
 }
 
 func LoadUnits() {
 	//BE CAREFUL ABOUT CHANGING THIS!!
-	UnitList[0] = Unit{Id: 0, Key: "stout_guard", DisplayName: "Bromuk", Description: "", Value: 5, Attributes: UnitAttributes{Def: 18, Amr: 2, Spd: 3, Hps: 15, WeaponIds: []int{5}}, CardText: ""}
-	UnitList[1] = Unit{Id: 1, Key: "stout_guard", DisplayName: "Stout Guard", Description: "", Value: 3, Attributes: UnitAttributes{Def: 18, Amr: 0, Spd: 3, Hps: 13, WeaponIds: []int{5}}}
+	AddUnit(Unit{Id: 0, Key: "stout_guard", DisplayName: "Bromuk", Description: "", Value: 5, Attributes: UnitAttributes{Def: 18, Amr: 2, Spd: 3, Hps: 15, WeaponIds: []int{5}}, CardText: ""})
+	AddUnit(Unit{Id: 1, Key: "stout_guard", DisplayName: "Stout Guard", Description: "", Value: 3, Attributes: UnitAttributes{Def: 18, Amr: 0, Spd: 3, Hps: 13, WeaponIds: []int{5}}})
 
-	UnitList[2] = Unit{Id: 2, Key: "archerguy", DisplayName: "Corath", Description: "", Value: 5, Attributes: UnitAttributes{Def: 15, Amr: 2, Spd: 4, Hps: 15, WeaponIds: []int{0, 2}}}
-	UnitList[3] = Unit{Id: 3, Key: "archerguy", DisplayName: "Mysterious Archer", Value: 4, Description: "", Attributes: UnitAttributes{Def: 15, Amr: 0, Spd: 3, Hps: 10, WeaponIds: []int{0, 2}}}
+	AddUnit(Unit{Id: 2, Key: "archerguy", DisplayName: "Corath", Description: "", Value: 5, Attributes: UnitAttributes{Def: 15, Amr: 2, Spd: 4, Hps: 15, WeaponIds: []int{0, 2}}})
+	AddUnit(Unit{Id: 3, Key: "archerguy", DisplayName: "Mysterious Archer", Value: 4, Description: "", Attributes: UnitAttributes{Def: 15, Amr: 0, Spd: 3, Hps: 10, WeaponIds: []int{0, 2}}})
 
-	UnitList[4] = Unit{Id: 4, Key: "spearman", DisplayName: "Wild Spearman", Value: 5, Description: "", Attributes: UnitAttributes{Keywords: []string{"+2/+3 v mounted"}, Def: 16, Amr: 0, Spd: 4, Hps: 14, WeaponIds: []int{4}}}
-	UnitList[6] = Unit{Id: 6, Key: "archerguy", DisplayName: "Wild Hunter", Value: 5, Description: "", Attributes: UnitAttributes{Def: 14, Amr: 0, Spd: 4, Hps: 11, WeaponIds: []int{1}}}
+	AddUnit(Unit{Id: 4, Key: "spearman", DisplayName: "Wild Spearman", Value: 5, Description: "", Attributes: UnitAttributes{Keywords: []string{"+2/+3 v mounted"}, Def: 16, Amr: 0, Spd: 4, Hps: 14, WeaponIds: []int{4}}})
 
-	UnitList[5] = Unit{Id: 5, Key: "mounted_knight", DisplayName: "Thunder Cavalry", Value: 6, Attributes: UnitAttributes{Keywords: []string{"mounted"}, Def: 13, Amr: 1, Spd: 5, Hps: 10, WeaponIds: []int{2, 3}}}
+	AddUnit(Unit{Id: 5, Key: "mounted_knight", DisplayName: "Thunder Cavalry", Value: 6, Attributes: UnitAttributes{Keywords: []string{"mounted"}, Def: 13, Amr: 1, Spd: 5, Hps: 10, WeaponIds: []int{2, 3}}})
+	AddUnit(Unit{Id: 6, Key: "archerguy", DisplayName: "Wild Hunter", Value: 5, Description: "", Attributes: UnitAttributes{Def: 14, Amr: 0, Spd: 4, Hps: 11, WeaponIds: []int{1}}})
+
 }
 
 func LoadSquads() {
-	SquadList[0] = Squad{Id: 0, Cost: 10, DisplayName: "Bromuk's Defenders", LeaderId: 0, GruntIds: []int{1, 1, 1}, Factions: []int{0, 1, 2}}
-	SquadList[1] = Squad{Id: 1, Cost: 15, DisplayName: "Corath's Rangers", LeaderId: 2, GruntIds: []int{3, 3}, Factions: []int{0, 1, 2}}
-	SquadList[2] = Squad{Id: 2, Cost: 10, DisplayName: "Lost Hunters", LeaderId: 6, GruntIds: []int{4, 4}, Factions: []int{0, 1, 2}}
-	SquadList[3] = Squad{Id: 3, Cost: 18, DisplayName: "The Thunder Cavalry", LeaderId: 5, GruntIds: []int{5, 5}, Factions: []int{0, 1, 2}}
+	AddSquad(Squad{Id: 0, Cost: 10, DisplayName: "Bromuk's Defenders", LeaderId: 0, GruntIds: []int{1, 1, 1}, Factions: []int{0, 1, 2}})
+	AddSquad(Squad{Id: 1, Cost: 15, DisplayName: "Corath's Rangers", LeaderId: 2, GruntIds: []int{3, 3}, Factions: []int{0, 1, 2}})
+	AddSquad(Squad{Id: 2, Cost: 10, DisplayName: "Lost Hunters", LeaderId: 6, GruntIds: []int{4, 4}, Factions: []int{0, 1, 2}})
+	AddSquad(Squad{Id: 3, Cost: 18, DisplayName: "The Thunder Cavalry", LeaderId: 5, GruntIds: []int{5, 5}, Factions: []int{0, 1, 2}})
+}
+
+func AddWeapon(w Weapon) {
+	WeaponList = append(WeaponList, w)
+}
+
+func AddUnit(u Unit) {
+	UnitList = append(UnitList, u)
+}
+
+func AddSquad(s Squad) {
+	SquadList = append(SquadList, s)
 }
 
 func GetUnit(id int) Unit {
-	ret := UnitList[id]
+	var ret Unit
+	for _, u := range UnitList {
+		if u.Id == id {
+			ret = u
+		}
+	}
+	return ret
+}
+
+func GetWeapon(id int) Weapon {
+	var ret Weapon
+	for _, w := range WeaponList {
+		if w.Id == id {
+			ret = w
+		}
+	}
 	return ret
 }
 
 func CreateUnit(id int) Unit {
-	ret := UnitList[id]
+	ret := GetUnit(id)
 	ret.Uid = uuid.NewV4()
 	for _, w := range ret.Attributes.WeaponIds {
-		nw := WeaponList[w]
-		fmt.Println(nw.DisplayName)
+		nw := GetWeapon(w)
 		nw.Uid = uuid.NewV4()
 		ret.Attributes.Weapons = append(ret.Attributes.Weapons, nw)
 	}
@@ -118,12 +146,17 @@ func CreateUnit(id int) Unit {
 }
 
 func GetSquad(id int) Squad {
-	ret := SquadList[id]
+	var ret Squad
+	for _, s := range SquadList {
+		if s.Id == id {
+			ret = s
+		}
+	}
 	return ret
 }
 
 func CreateSquad(id int) Squad {
-	ret := SquadList[id]
+	ret := GetSquad(id)
 	ret.Uid = uuid.NewV4()
 	for i := range ret.GruntIds {
 		ret.Grunts = append(ret.Grunts, CreateUnit(ret.GruntIds[i]))
