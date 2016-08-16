44560f73a6682b844bc86e1da1eafde68f5f5148
Andrew Nissen <adnissen@protonmail.com>
2016-08-16 17:31:55 -0400

    start uploading mac version

--- client/build.sh
+++ client/build.sh
@@ -7,8 +7,9 @@ electron-packager . --all --out=build --overwrite
 echo BUILD COMPLETE, UPLOADING TO ITCH
 echo keep an eye out, you may be prompted!
 echo ...
-butler push build/ElderRune-win32-ia32 ambushsabre/elder-runes:win-pre-alpha
-butler push build/ElderRune-linux-ia32 ambushsabre/elder-runes:linux-pre-alpha
+butler push build/ElderRunes-win32-ia32 ambushsabre/elder-runes:win-pre-alpha
+butler push build/ElderRunes-linux-ia32 ambushsabre/elder-runes:linux-pre-alpha
+butler push build/ElderRunes-darwin-x64 ambushsabre/elder-runes:mac-pre-alpha
 
 echo PUSH COMPLETE!
 echo TIME TO PARTY!!!!!!!!!!
